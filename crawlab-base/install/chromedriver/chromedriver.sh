3e32130b879afa65d7780ce815260cf7f1667021
zhgqthomas <zhgqthomas@outlook.com>
2024-12-02 07:48:58 +0800

    Update chromedriver.sh

--- crawlab-base/install/chromedriver/chromedriver.sh
+++ crawlab-base/install/chromedriver/chromedriver.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-version="131.0.6778.86"
 
+# Latest stable Chrome version as of March 2024
+version="122.0.6261.128"
+
+# Set architecture
 ARCH=$(uname -m)
 case $ARCH in
     x86_64)
         CHROME_ARCH="amd64"
         DRIVER_ARCH="linux64"
         ;;
-    aarch64)
-        CHROME_ARCH="arm64"
-        DRIVER_ARCH="linux64_arm"
-        ;;
+    # aarch64)
+    #     CHROME_ARCH="arm64"
+    #     DRIVER_ARCH="linux64_arm"
+    #     ;;
     *)
         echo "Unsupported architecture: $ARCH"
         exit 1
@@ -23,7 +26,7 @@ apt-get install -y unzip xvfb libxi6 libgconf-2-4
 # Install Chrome
 wget -q "http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${version}-1_${CHROME_ARCH}.deb"
 apt-get -y install "./google-chrome-stable_${version}-1_${CHROME_ARCH}.deb"
-echo `google-chrome --version`
+echo "Chrome version: $(google-chrome --version)"
 rm -f "./google-chrome-stable_${version}-1_${CHROME_ARCH}.deb"
 
 # Install ChromeDriver
@@ -32,3 +35,6 @@ unzip "chromedriver_${DRIVER_ARCH}.zip"
 mv chromedriver /usr/local/bin/chromedriver
 chown root:root /usr/local/bin/chromedriver
 chmod +x /usr/local/bin/chromedriver
+
+# Verify installation
+echo "ChromeDriver version: $(chromedriver --version)"
